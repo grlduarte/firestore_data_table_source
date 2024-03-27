5cbb4ed347fba787508c1ce55bafc4831043c9c8
Gabriel Remus <gabriel.grld@gmail.com>
2024-03-27 02:55:36 -0300

    Test when 2 and 4 pages of data are available

--- test/firestore_data_table_source_test.dart
+++ test/firestore_data_table_source_test.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firestore_data_table_source/firestore_data_table_source.dart';
+
+Future<void> populateFirestore(
+  CollectionReference collection,
+  int numberOfRows,
+) async {
+  for (var i = 0; i < numberOfRows; i++) {
+    await collection.add({'lorem': 'ipsum'});
+  }
+}
 
 void main() async {
   const pageSize = 10;
@@ -9,6 +19,12 @@ void main() async {
   group('When created', () => firstInitialized(pageSize));
 
   group('When no data is available', () => noDataIsAvailable(pageSize));
+
+  group('When ${2 * pageSize} rows are available',
+      () => twoPagesAreAvailable(pageSize));
+
+  group('When ${4 * pageSize} rows are available',
+      () => fourPagesAreAvailable(pageSize));
 }
 
 void firstInitialized(int pageSize) {
@@ -52,6 +68,102 @@ void noDataIsAvailable(int pageSize) {
     test('it should be null', () => expect(sut.getRow(0), null));
     test('row count should be 0', () => expect(sut.rowCount, 0));
     test('row count should not be approximate',
+        () => expect(sut.isRowCountApproximate, false));
+  });
+}
+
+void twoPagesAreAvailable(int pageSize) {
+  late FirestoreDataTableSource sut;
+  final firestore = FakeFirebaseFirestore();
+
+  setUp(() async {
+    sut = FirestoreDataTableSource(
+      query: firestore.collection('lorem'),
+      getDataRow: (snapshot) => const DataRow(cells: []),
+      pageSize: pageSize,
+    );
+    await populateFirestore(firestore.collection('lorem'), 2 * pageSize);
+  });
+
+  tearDown(() => firestore.clearPersistence());
+
+  group('and first row is requested,', () {
+    setUp(() => sut.getRow(0));
+
+    test('it should be a DataRow', () => expect(sut.getRow(0), isA<DataRow>()));
+    test('row count should be ${2 * pageSize}',
+        () => expect(sut.rowCount, 2 * pageSize));
+    test('row count should be approximate',
+        () => expect(sut.isRowCountApproximate, true));
+  });
+
+  group('and row $pageSize is requested,', () {
+    setUp(() => sut.getRow(pageSize));
+
+    test('it should be a DataRow',
+        () => expect(sut.getRow(pageSize), isA<DataRow>()));
+    test('row count should be ${2 * pageSize}',
+        () => expect(sut.rowCount, 2 * pageSize));
+    test('row count should not be approximate',
+        () => expect(sut.isRowCountApproximate, false));
+  });
+}
+
+void fourPagesAreAvailable(int pageSize) {
+  late FirestoreDataTableSource sut;
+  final firestore = FakeFirebaseFirestore();
+
+  setUp(() async {
+    sut = FirestoreDataTableSource(
+      query: firestore.collection('lorem'),
+      getDataRow: (snapshot) => const DataRow(cells: []),
+      pageSize: pageSize,
+    );
+    await populateFirestore(firestore.collection('lorem'), 4 * pageSize);
+  });
+
+  tearDown(() => firestore.clearPersistence());
+
+  group('and first row is requested,', () {
+    setUp(() => sut.getRow(0));
+
+    test('it should be a DataRow', () => expect(sut.getRow(0), isA<DataRow>()));
+    test('row count should be ${2 * pageSize}',
+        () => expect(sut.rowCount, 2 * pageSize));
+    test('row count should be approximate',
+        () => expect(sut.isRowCountApproximate, true));
+  });
+
+  group('and row $pageSize is requested,', () {
+    setUp(() => sut.getRow(pageSize));
+
+    test('it should be a DataRow',
+        () => expect(sut.getRow(pageSize), isA<DataRow>()));
+    test('row count should be ${3 * pageSize}',
+        () => expect(sut.rowCount, 3 * pageSize));
+    test('row count should be approximate',
+        () => expect(sut.isRowCountApproximate, true));
+  });
+
+  group('and row ${2 * pageSize} is requested,', () {
+    setUp(() => sut.getRow(2 * pageSize));
+
+    test('it should be a DataRow',
+        () => expect(sut.getRow(2 * pageSize), isA<DataRow>()));
+    test('row count should be ${4 * pageSize}',
+        () => expect(sut.rowCount, 4 * pageSize));
+    test('row count should be approximate',
+        () => expect(sut.isRowCountApproximate, true));
+  });
+
+  group('and row ${3 * pageSize} is requested,', () {
+    setUp(() => sut.getRow(3 * pageSize));
+
+    test('it should be a DataRow',
+        () => expect(sut.getRow(3 * pageSize), isA<DataRow>()));
+    test('row count should be ${4 * pageSize}',
+        () => expect(sut.rowCount, 4 * pageSize));
+    test('row count should be approximate',
         () => expect(sut.isRowCountApproximate, false));
   });
 }
