2a5d0a90e50d9b24d025b212174cbc50cc453bd5
Gabriel Remus <gabriel.grld@gmail.com>
2024-03-27 02:53:52 -0300

    Test DataTableSource when no data is available

--- test/firestore_data_table_source_test.dart
+++ test/firestore_data_table_source_test.dart
@@ -7,6 +7,8 @@ void main() async {
   const pageSize = 10;
 
   group('When created', () => firstInitialized(pageSize));
+
+  group('When no data is available', () => noDataIsAvailable(pageSize));
 }
 
 void firstInitialized(int pageSize) {
@@ -28,4 +30,28 @@ void firstInitialized(int pageSize) {
   test('row count should be 0', () => expect(sut.rowCount, 0));
   test('row count should be approximate',
       () => expect(sut.isRowCountApproximate, true));
+}
+
+void noDataIsAvailable(int pageSize) {
+  late FirestoreDataTableSource sut;
+  final firestore = FakeFirebaseFirestore();
+
+  setUp(() {
+    sut = FirestoreDataTableSource(
+      query: firestore.collection('lorem'),
+      getDataRow: (snapshot) => const DataRow(cells: []),
+      pageSize: pageSize,
+    );
+  });
+
+  tearDown(() => firestore.clearPersistence());
+
+  group('and first row is requested,', () {
+    setUp(() => sut.getRow(0));
+
+    test('it should be null', () => expect(sut.getRow(0), null));
+    test('row count should be 0', () => expect(sut.rowCount, 0));
+    test('row count should not be approximate',
+        () => expect(sut.isRowCountApproximate, false));
+  });
 }
